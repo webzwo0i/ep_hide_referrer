727ef3d72cf0a1fa84919e3a57563bb55634c8b8
webzwo0i <webzwo0i@c3d2.de>
2015-07-25 22:43:13 +0200

    If a redirection host is specified we use it. If not, we activate a server-side redirect route and use it

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var path = require("path");
 var eejs = require('ep_etherpad-lite/node/eejs');
+var settings = require('ep_etherpad-lite/node/utils/Settings');
 
-exports.registerRoute = function (hook_name, args, callback) {
-  args.app.get('/redirect', function(req, res) {
-    console.log(__dirname);
-    res.send(eejs.require(__dirname + "/templates/redirect.html"));
-  });
+if (!settings.ep_hide_referrer) {
+  // use the redirect.html page
+  exports.registerRoute = function (hook_name, args, callback) {
+    args.app.get('/redirect', function(req, res) {
+      res.send(eejs.require(__dirname + "/templates/redirect.html"));
+    });
+  }
+}
+else if (settings.ep_hide_referrer.host) {
+  // use the configured redirection host
+  exports.clientVars = function(hook, context, callback)
+  {
+    return callback({ "redirectionHost": settings.ep_hide_referrer.host });
+  };
 }
--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,38 @@
 exports.postAceInit = function(hook, context){
+  var url = escape(e.currentTarget.href)
+
   $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").on('click', "a", function (e){
-    window.open('../redirect#'+escape(e.currentTarget.href));
-    e.preventDefault();
-    return false;
+    if(clientVars.redirectionHost){
+      openNewWindow(clientVars.redirectionHost+'?'+e.currentTarget.href); 
+    } 
+    else {
+      openNewWindow('../redirect#'+url);
+    }
   });
 
   $('#chattext > p').on('click', "a", function (e){
-    window.open('../redirect#'+escape(e.currentTarget.href));
-    e.preventDefault();
-    return false;
+    if(clientVars.redirectionHost){
+      openNewWindow(clientVars.redirectionHost+'?'+e.currentTarget.href); 
+    }
+    else {
+      openNewWindow('../redirect#'+url);
+    }
   });
 }
 
 exports.postTimesliderInit = function(hook, context){
   $('#padcontent').on('click', "a", function (e){
-    window.open('../../redirect#'+escape(e.currentTarget.href));
-    e.preventDefault();
-    return false;
+    if(clientVars.redirectionHost){
+      openNewWindow(clientVars.redirectionHost+'?'+e.currentTarget.href); 
+    }
+    else {
+      openNewWindow('../../redirect#'+url);
+    }
   });
+}
+
+function openNewWindow(url){
+  window.open(url);
+  e.preventDefault();
+  return false;
 }
